09260a0fa9dcf90eb71fdacf8980d9fef0fb46eb
MRollin03 <ththmr@itu.dk>
2026-03-27 14:00:39 +0100

    added Location/place method in the automaiton file

--- src/Utils/Automation.sh
+++ src/Utils/Automation.sh
@@ -2,15 +2,20 @@
 
 projectName="Aberdeen"
 
-# Coordinates
+# Coordinates or Location
 west=-2.2865
 south=57.0486
 east=-2.0428
 north=57.2185
 
+location="SOMEWHERE"
+
 echo "Starting Convertion of places!"
 
+# --- SELECT BETWEEN Place OR BBOX -----
+# python OSM2FeatherConverter.py --type PLACE --place $location --title $projectName &&
 python OSM2FeatherConverter.py --type BBOX --bbox $west $south $east $north --title $projectName &&
+
 python FEATHER/src/main.py --graph-input $projectName/FeatherEdges.csv --feature-input $projectName/featuresteis.csv --output ./$projectName/FeatherResult.csv && 
 python NodeEmbedding.py --title $projectName --input ./$projectName/FeatherResult.csv   --output ./$projectName
  
