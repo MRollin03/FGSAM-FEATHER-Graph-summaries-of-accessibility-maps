3f58d80b7f0b1daed8bec49daa73af6655540dd6
Morten <mofb@itu.dk>
2026-03-28 12:02:56 +0100

    minor syntax changes and made more like template

--- src/Utils/AutomationWin.bat
+++ src/Utils/AutomationWin.bat
@@ -1,28 +1,28 @@
 @echo off
 
-set projectName=Odense
+set projectName=...
 set baseDir=projects
-REM Select either BBOX or PLACE
-set MODE=PLACE
+:: Select either BBOX or PLACE
+set MODE=...
 
-REM bounding box
+:: bounding box
 set west=12.27259
 set south=56.10713
 set east=12.34640
 set north=56.13288
 
-REM Remember to replace the spaces with underscores ( _ )
-set location=Odense_Municipality
+:: Remember to replace the spaces with underscores ( _ )
+set location=...
 
 echo Starting Conversion of places!
-REM Kør konvertering baseret på MODE
-IF "%MODE%"=="PLACE" (
+:: Kør konvertering baseret på MODE
+if "%MODE%"=="PLACE" (
     python OSM2FeatherConverter.py --type %MODE% --place %location% --title %projectName% --output %baseDir%
-) ELSE (
+) else (
     python OSM2FeatherConverter.py --type %MODE% --bbox %west% %south% %east% %north% --title %projectName% --output %baseDir%
 )
 
-REM Fortsæt kun hvis de næste skridt lykkes
+:: Fortsæt kun hvis de næste skridt lykkes
 python FEATHER/src/main.py --graph-input "%baseDir%/%projectName%/FeatherEdges.csv" --feature-input "%baseDir%/%projectName%/featuresteis.csv" --output "%baseDir%/%projectName%/FeatherResult.csv"
 
 python NodeEmbedding.py --title %projectName% --input "%baseDir%/%projectName%/FeatherResult.csv" --output "%baseDir%/%projectName%"
