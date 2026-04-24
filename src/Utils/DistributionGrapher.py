78411bbc85d93cb1d9900c131435446be512bad6
Morten <mofb@itu.dk>
2026-04-24 21:38:01 +0200

    fixed issues and added split percentage graph

--- src/Utils/DistributionGrapher.py
+++ src/Utils/DistributionGrapher.py
@@ -62,6 +62,10 @@ color_arr = ["red", "blue", "green", "orange", "teal", "yellow", "grey", "brown"
 levels = ["High", "Medium-High", "Medium", "Medium-low", "Low"]
 
 data = pd.read_csv(CSV_FILE)
+feat_csv = pd.read_csv("./projects/" + LABEL_COL + "/featuresteis.csv")
+
+
+
 data = data.drop(columns =['id'])
 
 """Drop imaginary columns, keeping only the "real" ones"""
@@ -72,9 +76,7 @@ for col in data.columns:
 data = data.drop(columns=imaginary)
 
 def mean_for_single_feature_graph(feature):
-    cool = pd.read_csv("./projects/" + LABEL_COL + "/featuresteis.csv")
-
-    data_learning = pd.concat([data, cool], ignore_index=False, axis=1)
+    data_learning = pd.concat([data, feat_csv], ignore_index=False, axis=1)
 
     dist5 = (DISTANCE / 5) #make the increments flex with distance
     incrementor = 0 
@@ -125,8 +127,6 @@ def interpolate_dense(theta, y, factor=2):
     return theta_dense, cs(theta_dense)
 
 def Split_on_order(feature, eval_points=10):
-    cool = pd.read_csv("./projects/" + LABEL_COL + "/featuresteis.csv")
-
     data_feature = data.filter(like=feature)
 
     _, axes = plt.subplots(ORDER, 1, figsize=(10, 5 * ORDER))
@@ -135,7 +135,7 @@ def Split_on_order(feature, eval_points=10):
         end = int(((o+1) *eval_points))
 
         data_subset = data_feature.iloc[:,start:end]
-        data_subset = pd.concat([data_subset, cool.filter(like=feature)], ignore_index=False, axis=1)
+        data_subset = pd.concat([data_subset, feat_csv.filter(like=feature)], ignore_index=False, axis=1)
 
         sorted_data = data_subset.sort_values(by=[feature])
         sorted_data = sorted_data.drop(columns=[feature])
@@ -163,9 +163,7 @@ for feature in features:
     Split_on_order(feature)
 
 def percentage_split_graph():
-    feat = pd.read_csv("./projects/" + LABEL_COL + "/featuresteis.csv")
-
-    new_data = pd.concat([data, feat], ignore_index=False, axis=1)
+    new_data = pd.concat([data, feat_csv], ignore_index=False, axis=1)
     new_data['mean'] = new_data.mean(axis=1)
     sorted_data = new_data.sort_values('mean')
 
@@ -208,3 +206,55 @@ def percentage_split_graph():
     plt.savefig(OUTPUT + f"percentage")
 
 percentage_split_graph()
+
+def percentage_split_graph_split():
+    new_data = pd.concat([data, feat_csv], ignore_index=False, axis=1)
+    new_data['mean'] = new_data.mean(axis=1)
+    sorted_data = new_data.sort_values('mean')
+
+    long = sorted_data.shape[0]
+
+    sorted_data = sorted_data.drop(columns=features)
+
+    sorted_data = sorted_data.drop(columns=['mean'])
+
+    df_bottom = sorted_data.head(int(long * 0.1))
+    df_high = sorted_data.tail(int(long * 0.1))
+
+    mean_high = df_high.mean(axis=1).values   
+    mean_low  = df_bottom.mean(axis=1).values
+    std_high  = df_high.std(axis=1).values
+    std_low   = df_bottom.std(axis=1).values
+                            
+    theta = np.linspace(0, THETA_MAX, len(mean_high))
+    
+    theta_smooth, high_smooth = interpolate_dense(theta, mean_high, factor=100)
+    _, low_smooth = interpolate_dense(theta, mean_low, factor=100)
+    _, std_low_smooth = interpolate_dense(theta, std_low, factor=100)
+    _, high_std_smooth = interpolate_dense(theta, std_high, factor=100)
+    
+    _, ax = plt.subplots(2,figsize=(18, 12))
+            
+    ax[0].plot(theta_smooth, high_smooth, color='blue', label='Top 10%')
+    ax[0].fill_between(theta_smooth, high_smooth - high_std_smooth, high_smooth + high_std_smooth, color='blue', alpha=0.4)
+    ax[0].set_xlim(0, THETA_MAX)
+    ax[0].set_xlabel(r'Evaluation point: $\theta$')     
+    ax[0].set_ylabel("CF Value")
+    ax[0].set_ylim(-1.1,1.1) 
+    ax[0].set_title(f"mean distribution for 10% highest - {LABEL_COL}")
+    ax[0].legend()
+
+    ax[1].plot(theta_smooth, low_smooth, color='red', label='Bottom 10%')
+    ax[1].fill_between(theta_smooth, low_smooth - std_low_smooth, low_smooth + std_low_smooth, color='red', alpha=0.4)
+    ax[1].set_xlim(0, THETA_MAX)
+    ax[1].set_xlabel(r'Evaluation point: $\theta$')     
+    ax[1].set_ylabel("CF Value")
+    ax[1].set_ylim(-1.1,1.1) 
+    ax[1].set_title(f"mean distribution for 10% lowest - {LABEL_COL}")
+    ax[1].legend()
+
+    plt.tight_layout()
+    plt.savefig(OUTPUT + f"percentage_split")
+
+percentage_split_graph_split()
+
