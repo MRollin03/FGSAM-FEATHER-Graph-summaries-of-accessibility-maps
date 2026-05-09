cef41706d8a34835a4686f9b502d507e7626c25c
Morten <mofb@itu.dk>
2026-05-09 17:26:26 +0200

    renamed variables, trubo_grapher -> barGrapher and altered comments

--- src/Utils/DistributionGrapher.py
+++ src/Utils/DistributionGrapher.py
@@ -1,17 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.interpolate import CubicSpline
 
-
-CSV_FILE = "projects/daegu_20000_all/FeatherResult.csv"
-OUTPUT = "./projects/daegu_20000_all/"
-LABEL_COL = "daegu_20000_all"
+CSV_FILE = "projects/.../FeatherResult.csv"
+OUTPUT = "./projects/.../"
+LABEL_COL = "..."
 ORDER=5
-THETA_MAX=2.5
-DISTANCE = 3000
-# TIL Ordered graph
-eval_points = 25 #THIS IS VERY IMPORTANT TO HAVE CORRRRRECTTTTT
+# To Ordered graph
+eval_points = 25 
 dist = 20000 #this is for the plotting order graf, use the distance pandana was fed!
 features = ["outdoor_activities","learning","supplies","eating","moving","cultural_activities","physical_exercise","services","healthcare","financial","all_pois"]
 color_arr = ["red", "blue", "green", "orange", "teal", "yellow", "grey", "brown", "purple", "pink", "black"]
@@ -28,6 +24,8 @@ for col in data.columns:
     if "img" in col and col not in imaginary:
         imaginary.append(col)
 data = data.drop(columns=imaginary)
+
+#Create wave diagram for a given feature
 def Order_graph(feature):
     data_learning = pd.concat([data.reset_index(drop=True), feat_csv.reset_index(drop=True)],axis=1)
     tmp_lst = [] #make a list containing all "levels" of the order
@@ -35,7 +33,6 @@ def Order_graph(feature):
     data_learning = data_learning.filter(like=feature)
 
     for o in range(ORDER):
-
         start = int((o * eval_points))
         end = int(((o+1) *eval_points))
 
@@ -45,6 +42,7 @@ def Order_graph(feature):
         data_subset.drop(columns=[feature])
         tmp_lst.append(data_subset)
     _, axes = plt.subplots(5 ,1, figsize=(10, 3 * ORDER))
+    
     count = 0
     for df in tmp_lst:
         df = df.drop(columns=[feature])
@@ -65,49 +63,6 @@ def Order_graph(feature):
     
     plt.tight_layout()
     plt.savefig(OUTPUT + f"{feature}meanwave_ordered")
-    
-print("Making feature level split graphs")
-def mean_for_single_feature_graph(feature):
-    data_learning = pd.concat([data.reset_index(drop=True), feat_csv.reset_index(drop=True)],axis=1)
-
-    dist5 = (DISTANCE / 5) #make the increments flex with distance
-    incrementor = 0 
-
-    df_list = []
-
-    data_learning = data_learning.filter(like=feature)
-    
-    while incrementor < DISTANCE:
-        incrementor += dist5
-
-        tmp = data_learning[data_learning[feature] <= incrementor]
-        tmp = tmp.sort_values(by=[feature])
-        df_list.append(tmp)
-    
-        data_learning = data_learning[data_learning[feature] > incrementor]
-
-    _, axes = plt.subplots(5 ,1, figsize=(10, 3 * ORDER))
-    count = 0
-    for df in df_list:
-        df = df.drop(columns=[feature])
-        part_mean = df.mean(axis=1)
-
-        theta = np.linspace(0, THETA_MAX, len(part_mean))
-        std = df.std(axis=1)
-        axes[count].set_title(f"Mean distribution of {feature} - Level {levels[count]} - {LABEL_COL}")
-        axes[count].plot(theta, part_mean, color=color_arr[count], label=f"mean for {feature} ")
-        axes[count].fill_between(theta, part_mean - std, part_mean + std, color=color_arr[count], alpha=0.4)
-        axes[count].legend()
-
-        axes[count].set_xlim(0, THETA_MAX)
-        axes[count].set_xlabel(r'Evaluation point: $\theta$')     
-        axes[count].set_ylabel("CF Value")
-        axes[count].set_ylim(-1.1,1.1) 
-        count +=1
-    
-    plt.tight_layout()
-    plt.savefig(OUTPUT + f"{feature}meanwave")
-        
+      
 for feature in features:
-    mean_for_single_feature_graph(feature=feature)
     Order_graph(feature=feature)
